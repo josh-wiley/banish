94e423fbe9f55b4fa16237c906a9df389b4a41e8
Josh Wiley <josh@cloudbend.dev>
2022-10-19 20:35:34 -0700

    feat(numeric): initial training

---
+++ summon/dispel.py
@@ -0,0 +1,3 @@
+# add dispel function for numeric data
+# ensure inputs & outputs are compatible with numeric model
+# use automatic feature/output scaling/normalization
--- summon/model.py
+++ summon/model.py
@@ -1,31 +1,36 @@
 from torch import Tensor
 from torch.nn import Module, Embedding, Linear, BatchNorm1d, Tanh
 
+# create one model per data type
+# numeric, boolean, and categorical data types can be converted to numbers
+# free text data types require a vocabulary and should stored in a transformer
+# 1) inputs are row ID and column ID, single value output
+# 2) inputs are row ID, output is all related column values
 
-class Model(Module):
-    def __init__(self, rows: int, columns: int):
+# TODO: use stddev and other statistics to determine hidden layer size
+
+
+class NumericModel(Module):
+    def __init__(self, columns: int):
         super().__init__()
 
-        hidden_size = rows * columns
+        hidden_size = 10
 
         # input layer
-        self.C = Embedding(rows, columns)
-        self.linear1 = Linear(columns, hidden_size, bias=False)
+        self.linear1 = Linear(1, hidden_size, bias=False)
         self.batchnorm1 = BatchNorm1d(hidden_size)
         self.activation1 = Tanh()
 
         # hidden layer
-        self.linear2 = Linear(hidden_size, hidden_size)
+        self.linear2 = Linear(hidden_size, hidden_size, bias=False)
         self.batchnorm2 = BatchNorm1d(hidden_size)
         self.activation2 = Tanh()
 
         # output layer
         self.linear3 = Linear(hidden_size, columns)
-        self.batchnorm3 = BatchNorm1d(columns)
 
     def forward(self, x: Tensor) -> Tensor:
         # input layer
-        x = self.C(x)
         x = self.linear1(x)
         x = self.batchnorm1(x)
         x = self.activation1(x)
@@ -37,6 +42,5 @@ class Model(Module):
 
         # output layer
         x = self.linear3(x)
-        x = self.batchnorm3(x)
 
         return x
